c2ad90353cc8bd798721616898eef8fa35993119
Yonatan Kra <yonatan.kr@walkme.com>
2019-04-10 11:56:37 +0300

    Add the ball

--- index.js
+++ index.js
@@ -31,6 +31,8 @@ class AirHockey extends HTMLElement{
         this._canvas = this.shadowRoot.querySelector('canvas');
 
         this.setGoals();
+
+        this.ball = new AirHockeyBall();
     }
 
     setGoals() {
@@ -51,6 +53,30 @@ class AirHockey extends HTMLElement{
     }
 }
 
+class AirHockeyBall {
+
+    constructor(x, y, radius, speedX, speedY) {
+        this.x = x ? x : 0;
+        this.y = y ? y : 0;
+        this.radius = Number.isFinite(radius) ? radius : 10;
+        this.speedX = Number.isFinite(speedX) ? speedX : 2;
+        this.speedY = Number.isFinite(speedY) ? speedY : -2;
+    }
+
+    draw(canvas, advance) {
+        const ctx = canvas.getContext('2d');
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+        ctx.fillStyle = '#0095DD';
+        ctx.fill();
+        ctx.closePath();
+
+        if (advance !== false) {
+            this.x += this.speedX;
+            this.y += this.speedY;
+        }
+    }
+}
 window.customElements.define('air-hockey', AirHockey);
 
 const airHockey = document.createElement('air-hockey');
