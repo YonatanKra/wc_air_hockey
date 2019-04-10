86b0e3a977595ee7bb34108fb65f7ea2b138d664
Yonatan Kra <yonatan.kr@walkme.com>
2019-04-10 09:53:35 +0300

    Preliminary web component

--- index.js
+++ index.js
@@ -14,3 +14,26 @@ const templateString = `
 <canvas height="${DIMENSIONS.height}" width="${DIMENSIONS.width}"></canvas>
 `;
 template.innerHTML = templateString;
+
+class AirHockey extends HTMLElement{
+    _canvas;
+    ball;
+    players = {
+        right: null,
+        left: null
+    };
+
+    constructor() {
+        super();
+
+        this.attachShadow({mode: 'open'});
+        this.shadowRoot.appendChild(template.content.cloneNode(true));
+        this._canvas = this.shadowRoot.querySelector('canvas');
+    }
+}
+
+window.customElements.define('air-hockey', AirHockey);
+
+const airHockey = document.createElement('air-hockey');
+
+document.querySelector("#game").appendChild(airHockey);
